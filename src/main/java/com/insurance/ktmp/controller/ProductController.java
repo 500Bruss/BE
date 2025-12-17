b20cb2b8f7090cd356a78f0069ae0da2b4093a17
tuan-phat-affina <phat.tran@affina.com.vn>
2025-12-18 00:49:03 +0700

    test: all git action after fixed

--- src/main/java/com/insurance/ktmp/controller/ProductController.java
+++ src/main/java/com/insurance/ktmp/controller/ProductController.java
@@ -15,10 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-import static org.reflections.Reflections.log;
-
 
 @RestController
 @RequestMapping("/api/products")
